99bce3ccb7d12b699235cfc37fe40edf40d9a4d4
xializhi <xializhi@jujiaotech.com>
2023-05-25 17:20:01 +0800

    feat: jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+// import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @ApiTags('user')
 @Controller('user')
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @ApiOperation({ summary: '登录' })
   @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { PostsService } from './posts.service';
 import { PostsEntity } from './posts.entity';
 import { ApiTags, ApiParam, ApiOperation } from '@nestjs/swagger';
@@ -9,12 +10,14 @@ import { CreatePostDto } from './dto/create-post.dot';
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @ApiOperation({ summary: '获取所有数据' })
   @Get('getAllMess')
   getAllMess(): object {
     return this.postsService.getAllMess();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @ApiOperation({ summary: '单个插入数据' })
   @Post('insetMess')
   @ApiParam({ name: 'postMess', type: Object, description: 'postMess' })
@@ -22,6 +25,7 @@ export class PostsController {
     return await this.postsService.insertMess(post);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @ApiOperation({ summary: '批量插入数据' })
   @Post('batchInsetMess')
   @ApiParam({ name: 'postMessArr', type: Array, description: 'postMessArr' })
@@ -31,12 +35,14 @@ export class PostsController {
     return await this.postsService.batchInsetMess(post);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @ApiOperation({ summary: '通过json文件插入数据' })
   @Post('insertMessByJson')
   async insertMessByJson(): Promise<Partial<PostsEntity>[]> {
     return await this.postsService.insertMessByJson();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @ApiOperation({ summary: '通过id删除数据' })
   @Get('delMess')
   @ApiParam({ name: 'id', type: String, description: 'id' })
